1ff97630bc649f61984608c36cf1832fe95a65eb
toharikr <53942441+toharikr@users.noreply.github.com>
2020-06-21 22:13:56 -0400

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello World! Welcome to AKS! Releas";
+        return "Hello World! Welcome to AKS! Relea";
     }
 
     public static void main(String[] args) {
