93f4528ecfda4fed5fe78bb724a65a08b88984e8
toharikr <53942441+toharikr@users.noreply.github.com>
2020-06-22 11:06:33 -0400

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,12 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+ import com.microsoft.applicationinsights.TelemetryClient;
+ import java.io.IOException;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
+ import com.microsoft.applicationinsights.telemetry.Duration;
+
+
 @SpringBootApplication
 @RestController
 public class Application {
+    
+     @Autowired
+     TelemetryClient telemetryClient;
 
     @RequestMapping("/")
     public String home() {
+        
+         telemetryClient.trackEvent("Sending a custom event...");
+
+         //trace a custom trace
+         telemetryClient.trackTrace("Sending a custom trace....");
+
+         //track a custom metric
+         telemetryClient.trackMetric("custom metric", 1.0);
+        
         return "Hello World! Welcome to AKS! with Approvers";
     }
 
